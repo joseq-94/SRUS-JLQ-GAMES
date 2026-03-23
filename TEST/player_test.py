9fea1871dd243f3bb02a018e8207919c37edcd7c
Jose Luis Quintero <20149097@tafe.wa.edu.au>
2026-03-23 11:51:11 +0800

    change file of assessment-task

--- TEST/player_test.py
+++ TEST/player_test.py
@@ -11,5 +11,22 @@ class TestPlayer(unittest.TestCase):
         player = Player("1001", "Jose")
         self.assertEqual(player.name, "Jose")
 
+    def test_sort_players(self):
+        players = [Player('01',"Alice", 10),
+                   Player('02', "Bob", 5),
+                   Player('03', "Charlie", 15)]
+        # note: ensure initialization code is valid for **your** implementation.
+        # For example, is your parameter called uid? is the first parameter name?
+
+        # do **not** change the following code:
+        sorted_players = sorted(players)
+
+        # players must be sorted by score as shown here:
+        manually_sorted_players = [Player('02', "Bob", 5),
+                                   Player('01', "Alice", 10),
+                                   Player('03', "Charlie", 15)]
+
+        self.assertListEqual(sorted_players, manually_sorted_players)
+
 if __name__ == '__main__':
     unittest.main()
