d3021f041f05b128e6854d51317e2d9f3c74d8fe
Jose Luis <20149097@tafe.wa.edu.au>
2026-04-21 21:37:48 +0800

    add file player_bnode and node left and right

---
+++ APP/player_bnode.py
@@ -0,0 +1,32 @@
+import player
+
+
+class PlayerBNode:
+    def __init__(self):
+        self._Player = player
+        self.left = None
+        self.right = None
+
+    @property
+    def player(self):
+        return self._Player
+
+    @player.setter
+    def player(self, value):
+        self._Player = value
+
+    @property
+    def left(self):
+        return self._left
+
+    @left.setter
+    def left(self, node):
+        self._left = node
+
+    @property
+    def right(self):
+        return self._right
+
+    @right.setter
+    def right(self, node):
+        self._right = node
